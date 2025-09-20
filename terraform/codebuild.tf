5b0f05a0ce12f4d5773f788d89b5bef7c8c18823
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2025-09-20 17:18:57 +0530

    Update codebuild.tf

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -1,18 +1,21 @@
 resource "aws_codebuild_project" "app_build" {
-  name         = "${var.project_name}-codebuild"
+  name         = "${var.project_name}-build"
   service_role = aws_iam_role.codebuild_role.arn
+  description  = "Build Docker image and push to ECR"
 
   artifacts {
     type = "CODEPIPELINE"
   }
 
   environment {
-    compute_type                = "BUILD_GENERAL1_SMALL"
-    image                       = "aws/codebuild/standard:7.0"
-    type                        = "LINUX_CONTAINER"
-    privileged_mode             = true
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:7.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true  # Required for Docker builds
+
+    # Correct syntax: nested environment_variable block
     environment_variable {
-      name  = "ECR_REPO"
+      name  = "REPOSITORY_URI"
       value = aws_ecr_repository.app.repository_url
     }
   }
