b043d71bc48d4486f3f505ce30f88c3baa4e02a4
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2025-09-20 15:59:30 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,9 @@
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
 resource "aws_s3_bucket" "artifact_bucket" {
   bucket         = "${var.project_name}-artifacts-${random_id.suffix.hex}"
   force_destroy  = true
 }
 
-resource "aws_s3_bucket_acl" "artifact_bucket_acl" {
-  bucket = aws_s3_bucket.artifact_bucket.id
-  acl    = "private"
-}
