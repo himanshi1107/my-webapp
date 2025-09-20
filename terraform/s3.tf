62fbf36ffb84bcddfcc18c583e9de0705b59317d
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2025-09-20 21:14:28 +0530

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,3 +7,21 @@ resource "aws_s3_bucket" "artifact_bucket" {
   force_destroy  = true
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "artifact_lifecycle" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+
+  rule {
+    id     = "DeleteOldArtifacts"
+    status = "Enabled"
+
+    # Delete objects older than 30 days
+    expiration {
+      days = 5
+    }
+
+    # Apply to all objects in the bucket
+    filter {
+      prefix = ""  # empty prefix = all objects
+    }
+  }
+}
