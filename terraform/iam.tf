9c5afb51c1146c52ad46bf4b3743a992c127c6e3
Himanshi Bobde <71334315+himanshi1107@users.noreply.github.com>
2025-09-20 15:14:56 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -116,3 +116,8 @@ resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "${var.project_name}-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "ec2_codedeploy" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployFullAccess"
+}
